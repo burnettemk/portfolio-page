c5b7f46e92d71b57c21f4657fd8509e6807d83cd
burnettemk <wafflesxyz@gmail.com>
2023-08-30 16:13:14 -0700

    Created js test files

---
+++ src/js/ProgrammingExpCard.js
@@ -0,0 +1,10 @@
+const ProgrammingExpCard = () => {
+    // Return jsx
+    return `
+    <div>
+        <p>This is a paragraph inside of a div.</p>
+    </div>`
+    ;
+}
+
+export default ProgrammingExpCard;
