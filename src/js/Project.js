13ce2db9807e2299931619cf4adc84aa18103ca4
burnettemk <wafflesxyz@gmail.com>
2023-11-07 17:34:00 -0800

    Add project module

---
+++ src/js/Project.js
@@ -0,0 +1,15 @@
+class Project {
+    constructor (
+        name,
+        desc,
+        tools,
+        langs,
+    ) {
+        this.name = name;
+        this.desc = desc;
+        this.tools = tools;
+        this.langs = langs;
+    }
+}
+
+export default Project;
